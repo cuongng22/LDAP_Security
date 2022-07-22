d55895879722faefad126b8893371a2d908b9858
ducbinhquach <ducbinhquach@gmail.com>
2022-07-22 15:20:06 +0700

    first commit

---
+++ src/main/java/com/tecapro/ldap_jwt/LdapJwtApplication.java
@@ -0,0 +1,13 @@
+package com.tecapro.ldap_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LdapJwtApplication.class, args);
+    }
+
+}
